080d7a289e3d8b18b0f4189a6d4dd1bedf792530
Abirami Gopinath <abigopi584@gmail.com>
2017-10-25 19:31:13 -0500

    version 1.1

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -23,13 +23,46 @@ module.exports = function(app){
         res.sendFile(path.join(pathDir.dir+ '/src/client/index.html'));
 
     })
-    app.post('/loginForm',[bodyParserJSON,urlencoded],(req,res) =>{
+    app.post('/login',[bodyParserJSON,urlencoded],(req,res) =>{
         let loginInfo = req.body;
         res.send({status:200, loginnfo: loginInfo})
     })
 
     app.post('/registerUser',[bodyParserJSON,urlencoded], (req,res) =>{
         let userDetails = req.body;
-        console.log(userDetails);
+        console.log(userDetails)
+        res.status(200).send({message: "registration success "})
+        /*res.status(400).send({error: "duplicate username "})*/
+    })
+    app.post('/checkForEvents' ,[bodyParserJSON,urlencoded],(req,res)=>{
+        let eventsInfo = req.body.username;
+        /*res.status(404).send({anyEvents: false});*/
+        res.status(200).send({anyEvents: true, eventInfo: [{id: 1,
+                                                            event: 'Birthday',
+                                                            evtDesc: "Husband's Birthday",
+                                                            evtDate:'1982-05-11',
+                                                            evtReminder:true
+                                                            },{id: 2,
+                                                            event: 'Wedding',
+                                                            evtDesc: "Mom and Dad",
+                                                            evtDate:'1983-08-21',
+                                                            evtReminder:true
+                                                             },{id: 3,
+                                                                event: 'Wedding',
+                                                                evtDesc: "Abi and Balu",
+                                                                evtDate:'2018-12-08',
+                                                                evtReminder:true
+                                                            }
+        ]})
+    })
+/*    app.post('/createNewEvent',[bodyParserJSON,urlencoded],(req,res) => {
+        let event_username = req.body.username;
+        res.send({status: 400});
+       /!* res.send({status: 200, eventInfo:{event: 'birthday', eventDesc: 'husband birthday', dateofEvt:'05/11/2018', needReminder:'true'}})*!/
+
+    })*/
+
+    app.post('/UpdateEventDB',[bodyParserJSON,urlencoded], (req, res)=>{
+        res.status(200).send({anyEvents: true})
     })
 }
