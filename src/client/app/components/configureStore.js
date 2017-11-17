1ae5aed8bffbf21143c6e3977918df1aa3c22d26
Abirami Gopinath <abigopi584@gmail.com>
2017-11-17 14:19:19 -0500

    Update configure store using redux persist

--- src/client/app/components/configureStore.js
+++ src/client/app/components/configureStore.js
@@ -1,14 +1,28 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore,compose, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import storage from 'redux-persist/es/storage';
 import rootReducer from '../../reducers/rootreducer.js';
+import {persistStore, persistReducer } from 'redux-persist'
 
+const config ={
+    key: 'root',
+    storage
+}
 
+const reducer = persistReducer(config,rootReducer);
 function configureStore(){
-    return createStore(
-        rootReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-        applyMiddleware(thunk),
-    )
+    let store= createStore(
+        reducer,
+                            undefined,
+                            compose(
+                                applyMiddleware(thunk),
+                                window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+                            )
+                           )
+
+    let persistor =  persistStore(store);
+
+    return {store,persistor}
 }
 
 
