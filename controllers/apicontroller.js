1d9127b7b211b43c11499ab82d74625acbe5cc4e
Abirami Gopinath <abigopi584@gmail.com>
2017-12-22 12:42:02 -0500

    Update apicontroller.js

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var userInfo = require('../models/userInfoModel');
-var events = require('../models/eventModel');
-var path = require('path');
+let express = require('express');
+let app = express();
+let fs = require('fs');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let userInfo = require('../models/userInfoModel');
+let events = require('../models/eventModel');
+let path = require('path');
 
 module.exports = function(app){
     app.set('view engine', 'html');
@@ -17,6 +17,10 @@ module.exports = function(app){
     let urlencoded = bodyParser.urlencoded({extended: true});
     app.use(express.static(path.join(__dirname,'../src/client')));
 
+    app.get('*', function (request, response){
+        var pathDir = path.parse(__dirname);
+        response.sendFile(path.join(pathDir.dir, '/src/client/index.html'))
+    })
 
     app.get('/', (req,res) =>{
         var pathDir = path.parse(__dirname);
@@ -29,21 +33,59 @@ module.exports = function(app){
     })
 
     app.post('/registerUser',[bodyParserJSON,urlencoded], (req,res) =>{
-        let userDetails = req.body;
-        console.log(userDetails)
-        res.status(200).send({message: "registration success "})
+        console.log('re.body '+ JSON.stringify(req.body));
+        const username = req.body.username,
+              email = req.body.email,
+              password = req.body.password;
+
+
+        console.log('req.body.username '+ username);
+        // check if username is already so that duplicate userinfo is not created.
+
+        userInfo.find({username: username },(err,userInfoDb)=>{
+            console.log('inside db find')
+            if(err){
+                throw err;
+            }
+            else{
+                if(userInfoDb && userInfoDb.length ){
+                    console.log('if userInfo found '+ userInfoDb )
+                    res.status(403).send({message: "Duplicate Username"})
+                }
+                else{
+                    console.log('if userInfo not found')
+                    let newUserInfo =  new userInfo({
+                        username: username,
+                        email: email,
+                        password: password
+                    });
+                    newUserInfo.save((err,data) => {
+                        if(err){
+                            console.log('eer ' + err)
+                            throw err;
+                        }
+                        else{
+                            console.log('newUserInfo ' + newUserInfo)
+                            res.status(200).send({message: "registration success"})
+                        }
+                    })
+                }
+            }
+        })
+
+
         /*res.status(400).send({error: "duplicate username "})*/
     })
-    app.post('/checkForEvents' ,[bodyParserJSON,urlencoded],(req,res)=>{
+    app.post('/getEventsFromDB' ,[bodyParserJSON,urlencoded],(req,res)=>{
         let eventsInfo = req.body.username;
         /*res.status(404).send({anyEvents: false});*/
-        res.status(200).send({anyEvents: true, eventInfo: [{id: 1,
-                                                            event: 'Birthday',
-                                                            evtDesc: "Husband's Birthday",
-                                                            evtDate:'1982-05-11',
-                                                            evtReminder:true
-                                                            },{id: 2,
-                                                            event: 'Wedding',
+        res.status(200).send({ eventsInfo: [{id: 1,
+                                             event: 'Birthday',
+                                             evtDesc: "Husband's Birthday",
+                                             evtDate:'1982-05-11',
+                                             evtReminder:true
+                                              },{id: 2,
+                                               event: 'Wedding',
                                                             evtDesc: "Mom and Dad",
                                                             evtDate:'1983-08-21',
                                                             evtReminder:true
@@ -55,14 +97,19 @@ module.exports = function(app){
                                                             }
         ]})
     })
-/*    app.post('/createNewEvent',[bodyParserJSON,urlencoded],(req,res) => {
-        let event_username = req.body.username;
-        res.send({status: 400});
-       /!* res.send({status: 200, eventInfo:{event: 'birthday', eventDesc: 'husband birthday', dateofEvt:'05/11/2018', needReminder:'true'}})*!/
 
-    })*/
+    app.post('/checkEventsFromDB',[bodyParserJSON,urlencoded],(req,res)=>{
+        res.status(200).send({areThereAnyEvents : true});
+    })
 
     app.post('/UpdateEventDB',[bodyParserJSON,urlencoded], (req, res)=>{
-        res.status(200).send({anyEvents: true})
+        let eventInfo = {
+            id: 4,
+            event: req.body.eventInfo.event,
+            evtDesc: req.body.eventInfo.evtDesc,
+            evtDate:req.body.eventInfo.evtDate,
+            evtReminder:true
+        }
+        res.status(200).send({anyEvents: true })
     })
 }
